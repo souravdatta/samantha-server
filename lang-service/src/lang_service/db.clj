75963235d3aa7e61fe5aa44aed248ffe66e548fe
Sourav Datta <soura.jagat@gmail.com>
2019-05-19 18:00:29 +0530

    Add single place to connect to mongo. Add a second collection for queue based processing of text.

--- lang-service/src/lang_service/db.clj
+++ lang-service/src/lang_service/db.clj
@@ -4,14 +4,25 @@
             [monger.collection :as mc]))
 
 
-(defn save-query
+(def mongo-con (atom nil))
+
+
+(defn open-mongo-connection!
+  []
+  (swap! mongo-con mg/connect))
+
+
+(defn save-query!
   [response]
-  (let [con (mg/connect)
-        db (mg/get-db con (cfg :mongodb))
+  (let [db (mg/get-db @mongo-con (cfg :mongodb))
         uid (if (response :uid) (response :uid) 1)]
     (mc/insert db "queries" (merge response {:uid uid} {:processed false}))))
 
 
+(defn save-process-queue!
+  [uuid]
+  (let [db (mg/get-db @mongo-con (cfg :mongodb))]
+    (mc/insert db "process_queue" {:text_id uuid})))
     
 
 
--- lang-service/src/lang_service/handler.clj
+++ lang-service/src/lang_service/handler.clj
@@ -4,8 +4,9 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [lang-service.utils :refer [json-response md5]]
             [lang-service.rules :refer [match-all-rules]]
-            [lang-service.db :refer [save-query]])
-  (:import (java.util Date)))
+            [lang-service.db :refer [open-mongo-connection! save-query! save-process-queue!]])
+  (:import (java.util Date)
+           (java.util UUID)))
 
 
 (defn query-handler
@@ -16,8 +17,11 @@
                  {:query query} 
                  {:md5 (md5 (clojure.string/join " " (response :data)))}
                  {:datetime (Date.)}
+                 {:text_id (.toString (UUID/randomUUID))}
                  response)]
-    (save-query result)
+    (dosync
+      (save-query! result)
+      (save-process-queue! (result :text_id)))
     (json-response result)))
 
 
@@ -29,5 +33,7 @@
 
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (do
+    (open-mongo-connection!)
+    (wrap-defaults app-routes site-defaults)))
 
